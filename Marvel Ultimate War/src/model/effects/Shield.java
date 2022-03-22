6cf67412f9dde4392351e83cb22a8ef262b6423f
Rambo <smido964@gmail.com>
2022-03-22 23:31:12 +0200

    Uploading the game so far

---
+++ Marvel Ultimate War/src/model/effects/Shield.java
@@ -0,0 +1,10 @@
+package model.effects;
+
+public class Shield extends Effect{
+	public Shield(String name, int duration , EffectType type) {
+		super(name , duration, type);
+		type = EffectType.BUFF;
+		
+	}
+
+}
---
+++ Marvel Ultimate War/src/model/world/Villian.java
@@ -0,0 +1,10 @@
+package model.world;
+
+public class Villian extends Champion{
+	public Villian(String name, int maxHP, int mana, int maxActions, int speed, int attackRange,
+    		int attackDamage) {
+		super(name, maxHP, mana, maxActions, speed, attackRange, attackDamage );
+
+	}
+	
+}
