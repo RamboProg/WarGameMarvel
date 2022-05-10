f0919dd4fef8cbfa3607d6a9813ad51a00563a72
Rambo <smido964@gmail.com>
2022-05-10 08:48:18 +0200

    effects 1 to 5

--- Marvel Ultimate War/src/model/effects/Root.java
+++ Marvel Ultimate War/src/model/effects/Root.java
@@ -1,9 +1,25 @@
 package model.effects;
 
+import model.world.Champion;
+
 public class Root extends Effect{
 	public Root(int duration) {
 		super("Root" , duration, EffectType.DEBUFF);
-		//type = EffectType.DEBUFF;
 	}
+	
+	public void apply(Champion c) {
+		c.getAppliedEffects().add(new Root(getDuration()));
+	}
+
+	public void remove(Champion c) {
+		for (Effect effect : c.getAppliedEffects()) {
+			if (effect.getName().equals("Root")) {
+				c.getAbilities().remove(effect);
+				break;
+			}
+		}
+		
+	}
+	
 
 }
