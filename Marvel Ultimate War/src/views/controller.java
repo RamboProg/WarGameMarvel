ab0923b78ad6f2de515b19f512ae0e4b187035e8
Rambo <smido964@gmail.com>
2022-05-29 11:49:02 +0200

    V0.5.1 (Initial start to User entry stuff)

--- Marvel Ultimate War/src/views/controller.java
+++ Marvel Ultimate War/src/views/controller.java
@@ -1,5 +1,37 @@
 package views;
 
-public class controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import engine.Game;
+
+
+public class Controller implements ActionListener{
+    
+    private Game model;
+    private View view;
+
+    public Controller(){
+        model = new Game(model.getFirstPlayer(), model.getSecondPlayer());
+        
+        //Passing the controller as a parameter (Passing itself)
+        view = new View(this); 
+        Object[][] board = model.getBoard();
+        view.updateBoard(board);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public static void main(String[] args) {
+		new Controller();
+	}
+
+
+
     
 }
--- Marvel Ultimate War/src/views/view.java
+++ Marvel Ultimate War/src/views/view.java
@@ -1,5 +1,60 @@
 package views;
 
-public class view {
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.Popup;
+import javax.swing.PopupFactory;
+
+import engine.Game;
+
+public class View extends JFrame {
+    private ActionListener listener;
+	private JPanel board;
+    private JPanel champChoice;
+
+    public View(ActionListener l){
+        this.listener = l;
+
+        setTitle("Marvel War Game");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setExtendedState(JFrame.MAXIMIZED_BOTH); //Maximises screen
+		setBounds(0, 50, 1280, 720);//when i de-maximise
+		setVisible(true);
+
+        board = new JPanel(new GridLayout(Game.getBoardheight(), Game.getBoardwidth()));
+    }
+
+    public void popUpChampCoice(){
+        champChoice = new JPanel(new GridLayout());
+
+    }
+
+    public void popUpUserEntry() {
+        JTextField firstName = new JTextField(20);
+        JLabel label1 = new JLabel("Enter 1st Player's name: ");
+
+        JTextField secondName = new JTextField(20);
+        JLabel label2 = new JLabel("Enter the 2nd Player's name: ");
+
+	}
+
+    public void updateBoard(Object[][] board) {
+        //TO-DO Figure out what to put here
+        
+        
+        //Refresh and tell java that sth changed
+		this.add(this.board);
+		this.revalidate();
+		this.repaint();
+    }
+
     
 }
